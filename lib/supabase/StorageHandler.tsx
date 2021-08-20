f082e9f7090ce2d07c73ba0f50f8d06b86ddc6be
Juan Quispe <jquispeh63@gmail.com>
2021-08-20 09:03:18 -0500

    Complete functionality of image update
    - Finally implements real updating for the image in the database
    - Name changes in variables are due to the need for two useAsync
    - createObjectURL wasn't returning a valid src, replaced with FileReader

--- lib/supabase/StorageHandler.tsx
+++ lib/supabase/StorageHandler.tsx
@@ -1,9 +1,25 @@
 import Supabase from './Supabase'
 
-export async function getImage(id:string) {
+export async function getImage(id:string):Promise<string> {
   const { data, error } = await Supabase.storage.from('images').download(id)
 
   if (error) throw error
+  if(!data) throw new Error('Image is null')
 
-  return URL.createObjectURL(data)
+  return await new Promise(resolve => {
+    const reader = new FileReader()
+    reader.onload = () => resolve(reader.result as string)
+    reader.readAsDataURL(data)
+  })
+}
+
+export async function updateImage(id:string, image:File | null) {
+  if(image) {
+    const { error } = await Supabase.storage.from('images').upload(id, image)
+
+    if (error) throw error
+
+    return
+  }
+  throw new Error('Image is null')
 }
--- pages/images/[id].tsx
+++ pages/images/[id].tsx
@@ -4,7 +4,7 @@ import JButton from '@Components/JButton'
 import useAsync from '@Hooks/useAsync'
 import useClipboard from '@Hooks/useClipboard'
 import Editor from '@Routing/Editor'
-import { getImage } from 'lib/supabase/StorageHandler'
+import { getImage, updateImage } from 'lib/supabase/StorageHandler'
 import styles from '@Styles/Editor.module.scss'
 import imagesStyles from './styles.module.scss'
 import type { imageQuery } from '@Types'
@@ -12,31 +12,32 @@ import type { imageQuery } from '@Types'
 function Images({ id }:imageQuery) {
   const { copyToClipboard } = useClipboard()
   const input = useRef<HTMLInputElement>(null)
-  const { execute, value } = useAsync(() => getImage(id))
-  const [ url, setUrl ] = useState<string | null>()
+  const [ url, setUrl ] = useState<string | null>(null)
+  const [ file, setFile ] = useState<File | null>(null)
+  const { execute:loadImage, value:imgValue } = useAsync(() => getImage(id))
+  const { execute, value } = useAsync(() => updateImage(id, file))
 
   useEffect(() => {
-    execute()
+    loadImage()
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   useEffect(() => {
-    if(value.status === 'success') {
-      setUrl(value.response)
-      console.log(value.response)
+    if(imgValue.status === 'success') {
+      setUrl(imgValue.response)
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value.status])
+  }, [imgValue.status])
 
-  function loadImage(e:ChangeEvent<HTMLInputElement>){
+  function setImage(e:ChangeEvent<HTMLInputElement>){
     if(e.target.files) {
       const file = e.target.files[0]
       
       if(['image/gif', 'image/jpeg', 'image/png'].includes(file.type)) {
         const reader = new FileReader()
         reader.onload = () => {
-          console.log(reader.result)
           setUrl(reader.result as string | null)
+          setFile(file)
         }
         reader.readAsDataURL(file)
       }
@@ -68,16 +69,19 @@ function Images({ id }:imageQuery) {
             {url ?
             <Image src={url} layout='fill' alt={id} />
             :
-            (value.status === 'success') && <Image src={value.response} layout='fill' alt={id} />}
+            (imgValue.status === 'success') && <Image src={imgValue.response} layout='fill' alt={id} />}
           </div>
           <div className={imagesStyles.imageUploader}>
             <JButton onClick={() => input.current?.click()}>Upload Image</JButton>
-            <input ref={input} type='file' onChange={loadImage}  />
+            <input ref={input} type='file' onChange={setImage}  />
           </div>
         </div>
       </div>
       <div className={styles.editorActions}>
-        <JButton>Update</JButton>
+        <JButton onClick={execute} disabled={!(file && value.status !== 'pending')}>Update</JButton>
+        {value.status === 'pending' && <p>Updating values...</p>}
+        {value.status === 'success' && <p>Values successfully updated.</p>}
+        {value.status === 'error' && <p>There&apos;s been an error ({value.error})</p>}
       </div>
     </div>
   )
